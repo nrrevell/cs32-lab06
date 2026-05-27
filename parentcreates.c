6484daa1ebde023150af5d92937666680583ed93
Noah <nrevell@ucsb.edu>
2026-05-26 17:52:23 -0700

    again, i have no idea if this is right

--- parentcreates.c
+++ parentcreates.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 
 int main(int argc, char **argv) {
@@ -8,23 +9,21 @@ int main(int argc, char **argv) {
         fprintf(stderr, "Usage: forkloop <iterations>\n");
         exit(1);
     }
-
+    int archparent = getppid();
     int iterations = strtol(argv[1], NULL, 10);
-    int n = 10;
-    for (int i = 1; i < iterations; i++) {
-        if (n > 0) {
+    int n = archparent;
+    for (int i = 0; i < iterations; i++) {
+        if (n==archparent) {
             n = fork();
-            //printf("oh i'm forking it because i'm a parent, n is %d\n", n);
-        }
-        if (n == 0) {
-            //printf("i'm not forking it because i'm a child, n is %d\n", n);
         }
         if (n < 0) {
             perror("fork");
             exit(1);
         }
+
         printf("ppid = %d, pid = %d, i = %d\n", getppid(), getpid(), i);
     }
+    while(wait(NULL) > 0);
 
     return 0;
 }
